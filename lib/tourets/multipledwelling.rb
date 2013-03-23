d69bba9ac6445f962776c44e5f22b5706172993f
Justin-Mitchell <jmitchell4140@gmail.com>
2013-03-22 18:13:57 -0700

    changed multiple prop-types to giant single hash

--- lib/tourets/multipledwelling.rb
+++ lib/tourets/multipledwelling.rb
@@ -31,7 +31,7 @@ module TouRETS
       def where(search_params = {})
         TouRETS.ensure_connected!
         [].tap do |properties|
-          search_params = map_mul_search_params(SEARCH_QUERY_DEFAULTS.merge(search_params))
+          search_params = map_search_params(SEARCH_QUERY_DEFAULTS.merge(search_params))
           search_config = SEARCH_CONFIG_DEFAULTS.merge({:query => hash_to_rets_query_string(search_params)})
           Search.find(search_config) do |property|
             properties << self.new(property)
