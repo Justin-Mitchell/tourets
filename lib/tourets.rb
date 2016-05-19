bf6616bbb3fdc572961e1ece3e04af5effe3e41d
Justin-Mitchell <jmitchell4140@gmail.com>
2016-05-18 19:08:52 -0700

    requiring new class into tourets.rb

--- lib/tourets.rb
+++ lib/tourets.rb
@@ -3,6 +3,7 @@ require 'tourets/rails'
 require 'tourets/utilities'
 require 'tourets/exceptions'
 require 'tourets/extensions/hash' # This should go away after Rails 4.0
+require 'tourets/property'
 require 'tourets/search'
 require 'tourets/photo'
 require 'tourets/residential'
@@ -15,14 +16,14 @@ require 'tourets/user'
 
 module TouRETS
   include Utilities
-  
+
   class << self
     attr_accessor :current_connection
-    
+
     # This will take the specific MLS you want to connect to
     # There must be a rets_config.yml file in the :rails_root/config directory
-    # 
-    def establish_connection(mls) 
+    #
+    def establish_connection(mls)
       close_connection if current_connection?
       config_file = File.join(app_root, 'config', 'rets_config.yml')
       if File.exists?(config_file)
@@ -39,21 +40,21 @@ module TouRETS
         raise ConfigurationError, "Couldn't find Config File in #{config_file}."
       end
     end
-    
+
     # Closes the current connection to the RETS server
     def close_connection
       current_connection.logout if current_connection?
       current_connection = nil
     end
-    
+
     def ensure_connected!
       raise ConnectionError, "You must establish a connection first." unless current_connection?
     end
-    
+
     def current_connection?
       !current_connection.nil?
     end
-    
+
     # The root of the application using this gem
     def app_root
       if defined?(Rails)
@@ -62,7 +63,7 @@ module TouRETS
         Dir.pwd
       end
     end
-    
+
   end
-  
+
 end
