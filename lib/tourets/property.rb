25753247fb658d1dc2f6a2dff0e3ad988da9b558
Justin-Mitchell <jmitchell4140@gmail.com>
2016-05-19 20:34:47 -0700

    mapped out a few new fields

--- lib/tourets/property.rb
+++ lib/tourets/property.rb
@@ -15,6 +15,7 @@ module TouRETS
           search_params = map_search_params(SEARCH_QUERY_DEFAULTS.merge(search_params))
           search_config = SEARCH_CONFIG_DEFAULTS.merge({:query => hash_to_rets_query_string(search_params)})
           Search.find(search_config) do |property|
+            #binding.pry
             properties << self.new(property)
           end
         end
@@ -22,5 +23,35 @@ module TouRETS
       end
 
     end
+
+    attr_accessor :attributes
+
+    def initialize(args = {})
+      self.attributes = args
+    end
+
+    # Return an array of the photo objects that belong to a particular property
+    def photos
+      @photos ||= grab_photos
+    end
+
+    # Look for one of the mapped keys, and return the value or throw method missing error.
+    def method_missing(method_name, *args, &block)
+      mapped_key = key_map[method_name.to_sym]
+      if attributes.has_key?(mapped_key)
+        attributes[mapped_key]
+      else
+        super
+      end
+    end
+
+    private
+
+      def grab_photos
+        [].tap do |pics|
+          pics << TouRETS::Photo.find(attributes['sysid'], :resource => :Property)
+        end.flatten
+      end
+
   end
 end
